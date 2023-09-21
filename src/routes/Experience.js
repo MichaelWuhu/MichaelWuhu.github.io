df605b77f80efba5dff29cc268f170370c68e550
Michael <mwu250@gmail.com>
2023-09-21 13:51:24 -0700

    created routes

---
+++ src/routes/Experience.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div>EXPERIENCE</div>
+    );
+}
+
+export default Experience;
---
+++ src/routes/Organizations.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Organizations = () => {
+    return (
+        <div>ORGANIZATIONS</div>
+    );
+}
+
+export default Organizations;
