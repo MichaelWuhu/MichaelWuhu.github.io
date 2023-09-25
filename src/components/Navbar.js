b1ecb6b5642785c0b66d39f142bd374b2eb0e348
Michael <mwu250@gmail.com>
2023-09-25 13:53:35 -0700

    started on homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,17 +11,17 @@ const Navbar = () => {
 
     return (
         <div className='header'>
-            <Link to='/'><h1>HEADER</h1></Link>
+            <Link to='/'><h1>MICHAEL WU</h1></Link>
             <ul className={ click ? "nav-menu active" : "nav-menu"}>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/About'>About</Link></li>
-                <li><Link to='/Experience'>Experience</Link></li>
+                <li><Link className='link' to='/'>Home</Link></li>
+                <li><Link className='link' to='/About'>About</Link></li>
+                <li><Link className='link' to='/Experience'>Experience</Link></li>
             </ul>
 
             <div className='hamburger' onClick={handleClick}>
                 { click ? 
-                    (<FaTimes size={20} style={{ color: '#fff' }} />) : 
-                    (<FaBars size={20} style={{ color: '#fff' }} />) }
+                    (<FaTimes size={20} style={{ color: 'black' }} />) : 
+                    (<FaBars size={20} style={{ color: 'black' }} />) }
             </div>
         </div>
     );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,15 @@
+import '../styles/Home.css';
+
 import React from 'react';
 
 const Home = () => {
+    const title = 'Hi. I\'m Michael.';
+    
     return (
-        <div>HOME</div>
+        <div className='Home'>
+            <div className='Title'>{title}</div>
+            <div className='Body'>something</div> 
+        </div>
     );
 }
 
