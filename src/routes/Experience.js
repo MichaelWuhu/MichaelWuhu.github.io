c631f85f0884c4c98bf3c6a8c8267116e5847e45
Michael <mwu250@gmail.com>
2024-02-05 22:39:03 -0800

    revamping

--- src/routes/Experience.js
+++ src/routes/Experience.js
@@ -1,28 +1,9 @@
 import "../styles/Experience.css";
 
-import ExperienceCard from "../components/ExperienceCard";
-import Footer from "../components/Footer";
-
-import sprocketLogo from "../images/sprocket.png"
-
 import React from "react";
 
 const Experience = () => {
-  return (
-    <div className="Experience">
-      <h1 className="Heading">Experience</h1>
-      <ExperienceCard
-        experienceTitle="Team 3473 Sprocket Robotics"
-        role="Software Engineer"
-        generalText="Team 3473 is Diamond Bar High School's competitive robotics team. 
-        On this team, I led the frontend development of the robot scouting app used by 
-        members to collect data on other team's robots. The data was then analyzed and 
-        visualized to help determine who we would form alliances with in the playoffs."
-        experienceImage={sprocketLogo}
-      />
-      <Footer />
-    </div>
-  );
+  return <div className="experience">Experience IN PROG</div>;
 };
 
 export default Experience;
